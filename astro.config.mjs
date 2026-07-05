ad8f20544f5a39cd5fad94a859558ea9b14259e0
IAMWIMP <2454355195@qq.com>
2026-07-05 14:55:28 +0800

    测试

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://ZhaoMingL.github.io",
+	site: "https://IAMWIMP.github.io",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
